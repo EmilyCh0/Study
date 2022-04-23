86aa23e89bd22ab30059b21e83d649e65f61b0c3
EmilyCh0 <seoyeon3089@naver.com>
2022-04-23 21:25:30 +0900

    spring boot api server

---
+++ ElasticsearchAPI/src/test/java/com/example/ElasticsearchAPI/ElasticsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ElasticsearchAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
